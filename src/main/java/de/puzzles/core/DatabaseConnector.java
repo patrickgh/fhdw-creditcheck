e7c24aab5c0bbb183f5c3f3020499954ab2fd4c9
Patrick Groß-Holtwick <p.grossholtwick@gmail.com>
2013-02-28 14:04:24 +0100

    added DatabaseConnector class added DatabaseConnector tests modified pom.xml to fix error messages

---
+++ src/main/java/de/puzzles/core/DatabaseConnector.java
@@ -0,0 +1,83 @@
+package de.puzzles.core;
+
+import java.nio.charset.Charset;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pgh
+ * Date: 28.02.13
+ * Time: 12:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class DatabaseConnector {
+
+    private static DatabaseConnector INSTANCE = new DatabaseConnector();
+    private Connection dbConnection = null; // connection vorbereiten
+
+    private DatabaseConnector() {
+        try {
+            // einen JDBC-Treiber registrieren und über den registrierten
+            // Treiber das Programm mit der DB verbinden:
+            Class.forName("com.mysql.jdbc.Driver");
+            dbConnection = DriverManager.getConnection("jdbc:mysql://localhost/puzzles", "root", "");
+        }
+        catch (ClassNotFoundException err) {
+            System.out.println("DB-Driver nicht gefunden!");
+            System.out.println(err);
+        }
+        catch (SQLException err) {
+            System.out.println("Connect nicht möglich");
+            System.out.println(err);
+        }
+
+    }
+
+    public static DatabaseConnector getInstance() {
+        return INSTANCE;
+    }
+
+    public boolean checkLogin(String user, String pw) {
+        String cryptedPassword = MD5(pw);
+        if (cryptedPassword != null) {
+            try {
+                // use PreparedStatement to prevent SQL-Injection (hopefully :))
+                PreparedStatement stmt = dbConnection.prepareStatement("SELECT username, password FROM consultants WHERE username LIKE ?");
+                stmt.setString(1, user);
+                stmt.execute();
+                ResultSet result = stmt.getResultSet();
+                if (result.next() && result.isLast()) {
+                    if (result.getString(2).equals(cryptedPassword)) {
+                        return true;
+                    }
+                }
+            }
+            catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    private String MD5(String md5) {
+        try {
+            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+            byte[] array = md.digest(md5.getBytes(Charset.forName("UTF-8")));
+            StringBuilder sb = new StringBuilder();
+            for (byte anArray : array) {
+                sb.append(Integer.toHexString((anArray & 0xFF) | 0x100).substring(1, 3));
+            }
+            return sb.toString();
+        }
+        catch (java.security.NoSuchAlgorithmException e) {
+            System.out.println("NoSuchAlgorithmException while creating MD5 hash");
+            System.out.println(e);
+        }
+        return null;
+    }
+
+}
---
+++ src/test/java/de/puzzles/core/DatabaseConnectorTest.java
@@ -0,0 +1,22 @@
+package de.puzzles.core;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pgh
+ * Date: 28.02.13
+ * Time: 13:31
+ * To change this template use File | Settings | File Templates.
+ */
+public class DatabaseConnectorTest {
+
+    @Test
+    public void testLogin() {
+        DatabaseConnector conn = DatabaseConnector.getInstance();
+        Assert.assertTrue(conn.checkLogin("bstinson", "test"));
+        Assert.assertFalse(conn.checkLogin("bstinson", "legendary"));
+    }
+
+}
