38837650b6a19c3f24b38f9cb23408521ee5895b
Patrick Groß-Holtwick <p.grossholtwick@gmail.com>
2013-03-14 20:18:28 +0100

    added required fields

--- src/main/java/de/puzzles/webapp/page/newrequest/steps/PersonalInformationStep.java
+++ src/main/java/de/puzzles/webapp/page/newrequest/steps/PersonalInformationStep.java
@@ -4,6 +4,7 @@ import com.vaynberg.wicket.select2.Select2Choice;
 import de.puzzles.core.domain.CreditRequest;
 import de.puzzles.core.domain.Customer;
 import de.puzzles.webapp.components.choiceprovider.ConsultantChoiceProvider;
+import org.apache.wicket.RuntimeConfigurationType;
 import org.apache.wicket.extensions.markup.html.form.DateTextField;
 import org.apache.wicket.extensions.wizard.WizardStep;
 import org.apache.wicket.markup.head.IHeaderResponse;
@@ -25,51 +26,52 @@ public class PersonalInformationStep extends WizardStep {
 
     public PersonalInformationStep(IModel<CreditRequest> defaultModel) {
         super();
+        boolean requireFields = System.getProperty("disableRequiredFields") == null;
 
         CompoundPropertyModel<CreditRequest> requestModel = new CompoundPropertyModel<CreditRequest>(defaultModel);
         CompoundPropertyModel<Customer> customerModel = new CompoundPropertyModel<Customer>(requestModel.getObject().getCustomer());
         TextField firstName = new TextField("firstName", customerModel.bind("firstname"));
-        firstName.setRequired(false);
+        firstName.setRequired(requireFields);
         add(firstName);
 
         TextField lastName = new TextField("lastName", customerModel.bind("lastname"));
-        lastName.setRequired(false);
+        lastName.setRequired(requireFields);
         add(lastName);
 
         birthdayField = new DateTextField("birthday", customerModel.<Date>bind("birthday"), "dd.MM.yyyy");
-        birthdayField.setRequired(false);
+        birthdayField.setRequired(requireFields);
         add(birthdayField);
 
         TextField street = new TextField("street", customerModel.bind("street"));
-        street.setRequired(false);
+        street.setRequired(requireFields);
         add(street);
 
         TextField zipCode = new TextField("zipcode", customerModel.bind("zipcode"));
-        zipCode.setRequired(false);
+        zipCode.setRequired(requireFields);
         add(zipCode);
 
         TextField city = new TextField("city", customerModel.bind("city"));
-        city.setRequired(false);
+        city.setRequired(requireFields);
         add(city);
 
         TextField telephone = new TextField("telephone", customerModel.bind("telephone"));
-        telephone.setRequired(false);
+        telephone.setRequired(requireFields);
         add(telephone);
 
         TextField email = new TextField("email", customerModel.bind("email"));
         email.add(EmailAddressValidator.getInstance());
-        email.setRequired(false);
+        email.setRequired(requireFields);
         add(email);
 
         TextField accountNumber = new TextField("accountnumber", customerModel.bind("accountnumber"));
         add(accountNumber);
 
         TextField bankCode = new TextField("bankcode", customerModel.bind("bankcode"));
-        bankCode.setRequired(false);
+        bankCode.setRequired(requireFields);
         add(bankCode);
 
         Select2Choice<Integer> consultant = new Select2Choice<Integer>("consultant", requestModel.<Integer>bind("consultantId"), new ConsultantChoiceProvider());
-        consultant.setRequired(false);
+        consultant.setRequired(requireFields);
         add(consultant);
 
     }
